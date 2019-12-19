27659a7375cb4444d4c795de2434a398be6f2e86
Neelesh Rajpurohit <nrajpurohit@tavisca.com>
2019-12-19 11:45:18 +0530

    added updated code for redux support

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-export const StateResolver= function (path, Ostate){
+import {combineReducers} from 'redux'
+import {composeWithDevTools} from 'redux-devtools-extension'
+import {createStore} from 'redux-dynamic-reducer'
+import {get,set} from 'dot-prop-immutable'
+
+class CoolStore {
+
+  createStore = createStore;
+  composeWithDevTools = composeWithDevTools;
+  get = get;
+  set = set;
+  combineReducers = combineReducers;
+
+  StateResolver = function (path, Ostate){
     let temp = path.split('.');
     for(let i=0;i<temp.length;i++){
       Ostate = Ostate[temp[i]];
     }
     return Ostate;
+  }
 }
+
+export default CoolStore;
